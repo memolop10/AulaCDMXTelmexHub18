6e5c6322238dd3713053ad70314155a20d073373
memolop <inglopezvargas@gmail.com>
2018-07-12 14:57:48 -0500

    blogs tarea

---
+++ memolop10/express-handlebars/controllerblogs.js
@@ -0,0 +1,62 @@
+let mongo = require("mongodb")
+let client = mongo.MongoClient
+const url = 'mongodb://localhost:27017'
+const dbName = 'Telmex'
+
+function findBlogByNum (req, res) {
+    client.connect(url, (err, conn) => {
+        if (err) console.log(err)
+        let db = conn.db(dbName)
+        db.collection('blogs')
+        .findOne(
+            {num: req.params.num},
+            function (err, data) {
+                res.render('blog', data)
+            }
+        )
+    })
+}
+
+function findAllBlogs (req, res) {
+    client.connect(url, (err, conn) => {
+        if (err) console.log(err)
+        let db = conn.db(dbName)
+        db.collection('blogs')
+        .find({})
+        .toArray(function (err, data) {
+            console.log(data)
+            res.send(data)
+        })
+    })
+}
+
+function renderNewBlog (req, res) {
+    res.render('new-blog')
+}
+function renderHome (req, res) {
+    res.render('home')
+}
+
+function insertBlog (req, res) {
+    console.log('body', req.body)
+    client.connect(url, (err, conn) => {
+        if (err) console.log(err)
+        let db = conn.db(dbName)        
+        db.collection('blogs')
+        .insert(
+            req.body,
+            function (err, data) {
+                console.log(data)
+                res.send(data)
+            }
+        )
+    })
+}
+
+module.exports = {
+    findAllBlogs,
+    findBlogByNum,
+    renderNewBlog,
+    renderHome,
+    insertBlog
+}
---
+++ memolop10/express-handlebars/public/js/main.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function (){
+    console.log('ya cargué el DOM')
+    $.get('/blogs', function (res) {
+        console.log(res)
+    })
+})
